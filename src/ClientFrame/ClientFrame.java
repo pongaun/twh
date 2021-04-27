3547065cdf3ef717e3946bf3690ca3d16ccebe9e
mmjj <komj5461@gmail.com>
2021-04-27 16:06:00 +0900

    채팅 예제(chatting test)

---
+++ src/ClientFrame/ClientFrame.java
@@ -0,0 +1,8 @@
+package ClientFrame;
+
+import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader; import java.io.BufferedWriter; import java.io.IOException; import java.io.InputStreamReader; import java.io.OutputStream; import java.io.OutputStreamWriter; import java.net.Socket; import javax.swing.JFrame; import javax.swing.JScrollPane; import javax.swing.JTextArea; import javax.swing.JTextField;
+
+출처: https://fors.tistory.com/187 [우종선]
